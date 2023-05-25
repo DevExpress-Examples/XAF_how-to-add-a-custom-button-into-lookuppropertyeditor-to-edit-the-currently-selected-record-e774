075a9f8e4d789fc531a2e4d1c0c768aec91f9a12
andrey <andrey.kozhevnikov@devexpress.com>
2023-05-25 18:55:32 +0400

    ef

---
+++ CS/EFCore/LookUpButtonEF/LookUpButtonEF.Module/BusinessObjects/LookUpButtonEFDbContext.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.ExpressApp.EFCore.Updating;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using DevExpress.Persistent.BaseImpl.EF.PermissionPolicy;
+using DevExpress.Persistent.BaseImpl.EF;
+using DevExpress.ExpressApp.Design;
+using DevExpress.ExpressApp.EFCore.DesignTime;
+using MySolution.Module.BusinessObjects;
+
+namespace LookUpButtonEF.Module.BusinessObjects;
+
+// This code allows our Model Editor to get relevant EF Core metadata at design time.
+// For details, please refer to https://supportcenter.devexpress.com/ticket/details/t933891.
+public class LookUpButtonEFContextInitializer : DbContextTypesInfoInitializerBase {
+	protected override DbContext CreateDbContext() {
+		var optionsBuilder = new DbContextOptionsBuilder<LookUpButtonEFEFCoreDbContext>()
+            .UseSqlServer(";")
+            .UseChangeTrackingProxies()
+            .UseObjectSpaceLinkProxies();
+        return new LookUpButtonEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+//This factory creates DbContext for design-time services. For example, it is required for database migration.
+public class LookUpButtonEFDesignTimeDbContextFactory : IDesignTimeDbContextFactory<LookUpButtonEFEFCoreDbContext> {
+	public LookUpButtonEFEFCoreDbContext CreateDbContext(string[] args) {
+		throw new InvalidOperationException("Make sure that the database connection string and connection provider are correct. After that, uncomment the code below and remove this exception.");
+		//var optionsBuilder = new DbContextOptionsBuilder<LookUpButtonEFEFCoreDbContext>();
+		//optionsBuilder.UseSqlServer("Integrated Security=SSPI;Pooling=false;Data Source=(localdb)\\mssqllocaldb;Initial Catalog=LookUpButtonEF");
+        //optionsBuilder.UseChangeTrackingProxies();
+        //optionsBuilder.UseObjectSpaceLinkProxies();
+		//return new LookUpButtonEFEFCoreDbContext(optionsBuilder.Options);
+	}
+}
+[TypesInfoInitializer(typeof(LookUpButtonEFContextInitializer))]
+public class LookUpButtonEFEFCoreDbContext : DbContext {
+	public LookUpButtonEFEFCoreDbContext(DbContextOptions<LookUpButtonEFEFCoreDbContext> options) : base(options) {
+	}
+    //public DbSet<ModuleInfo> ModulesInfo { get; set; }
+    public DbSet<MyTask> MyTasks { get; set; }
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.HasChangeTrackingStrategy(ChangeTrackingStrategy.ChangingAndChangedNotificationsWithOriginalValues);
+    }
+}
---
+++ CS/EFCore/LookUpButtonEF/LookUpButtonEF.Win/WinApplication.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ApplicationBuilder;
+using DevExpress.ExpressApp.Win;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.Win.Utils;
+using Microsoft.EntityFrameworkCore;
+using DevExpress.ExpressApp.EFCore;
+using LookUpButtonEF.Module;
+using LookUpButtonEF.Module.BusinessObjects;
+using System.Data.Common;
+
+namespace LookUpButtonEF.Win;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Win.WinApplication._members
+public class LookUpButtonEFWindowsFormsApplication : WinApplication {
+    public LookUpButtonEFWindowsFormsApplication() {
+		SplashScreen = new DXSplashScreen(typeof(XafSplashScreen), new DefaultOverlayFormOptions());
+        ApplicationName = "LookUpButtonEF";
+        CheckCompatibilityType = DevExpress.ExpressApp.CheckCompatibilityType.DatabaseSchema;
+        UseOldTemplates = false;
+        DatabaseVersionMismatch += LookUpButtonEFWindowsFormsApplication_DatabaseVersionMismatch;
+        CustomizeLanguagesList += LookUpButtonEFWindowsFormsApplication_CustomizeLanguagesList;
+    }
+    private void LookUpButtonEFWindowsFormsApplication_CustomizeLanguagesList(object sender, CustomizeLanguagesListEventArgs e) {
+        string userLanguageName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+        if(userLanguageName != "en-US" && e.Languages.IndexOf(userLanguageName) == -1) {
+            e.Languages.Add(userLanguageName);
+        }
+    }
+    private void LookUpButtonEFWindowsFormsApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+        e.Updater.Update();
+        e.Handled = true;
+#else
+        if(System.Diagnostics.Debugger.IsAttached) {
+            e.Updater.Update();
+            e.Handled = true;
+        }
+        else {
+			string message = "The application cannot connect to the specified database, " +
+				"because the database doesn't exist, its version is older " +
+				"than that of the application or its schema does not match " +
+				"the ORM data model structure. To avoid this error, use one " +
+				"of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
+
+			if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+				message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+			}
+			throw new InvalidOperationException(message);
+        }
+#endif
+    }
+}
